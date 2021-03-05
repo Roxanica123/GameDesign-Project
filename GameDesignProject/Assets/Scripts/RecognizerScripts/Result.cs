30537b39463e16e4047f2636151d3dc75c6ed8d2
Roxanica123 <irimia.andreea.roxana@gmail.com>
2021-03-05 17:08:39 +0200

    Scripts rewritten without Wobbrock lib

---
+++ GameDesignProject/Assets/Scripts/RecognizerScripts/Result.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Recognizer
+{
+    public class Result
+    {
+        private string name;
+        private float score;
+
+        public Result(string name, float score)
+        {
+            this.name = name;
+            this.score = score;
+        }
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public float Score
+        {
+            get => score;
+            set => score = value;
+        }
+    }
+}
---
+++ GameDesignProject/Assets/Scripts/RecognizerScripts/Unistroke.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Recognizer
+{
+    public class Unistroke
+    {
+        private string name;
+        private List<Point> points;
+        private float radians;
+
+        public Unistroke(string name, List<Point> points)
+        {
+            this.name = name;
+            this.points = points;
+            radians = MathUtility.IndicativeAngle(this.points);
+            this.points = MathUtility.RotateBy(this.points, -radians);
+            this.points = MathUtility.ScaleTo(this.points, (float) 250.0);
+            this.points = MathUtility.TranslateTo(this.points, new Point((float) 0.0, (float) 0.0));
+        }
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public List<Point> Points
+        {
+            get => points;
+            set => points = value;
+        }
+
+        public float Radians
+        {
+            get => radians;
+            set => radians = value;
+        }
+    }
+}
