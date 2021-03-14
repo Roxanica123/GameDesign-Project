3cbfc6099a0329eb22d1b9188a6ec4f97b4b3d5c
csum112 <cataremix15@gmail.com>
2021-03-14 20:13:02 +0200

    camera fix

--- GameDesignProject/Assets/Scripts/FiguresManager.cs
+++ GameDesignProject/Assets/Scripts/FiguresManager.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Notifications.iOS;
 using UnityEngine;
 
 public class FiguresManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Transform notesTransform;
+
+    private GameObject notePrefab;
+
+    private class Note
+    {
+        private float timeOfNote;
+        private GameObject reference;
+
+        public Note(float time)
+        {
+            timeOfNote = time;
+        }
+
+        public void Update()
+        {
+            
+        }
+    }
+    
+    
+
     void Start()
     {
-        
+        this.notesTransform = GameObject.Find("Notes").transform;
+        this.notePrefab = Resources.Load<GameObject>("Prefabs/Dummy");
     }
 
-    // Update is called once per frame
-    void Update()
+    private GameObject SpawnNote(Vector3 position)
     {
-        
+        var newNote = Instantiate(this.notePrefab, position, Quaternion.identity);
+        newNote.transform.SetParent(this.notesTransform);
+        return newNote;
     }
 }
