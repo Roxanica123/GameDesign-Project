a2dd15217d84971bd96845f68ebbc417e13f6e5e
csum112 <cataremix15@gmail.com>
2021-03-15 18:58:55 +0200

    feat: added a unity event to recognizer

--- GameDesignProject/Assets/Scripts/FiguresManager.cs
+++ GameDesignProject/Assets/Scripts/FiguresManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Security.Cryptography;
 using Unity.Notifications.iOS;
@@ -7,93 +8,94 @@ using UnityEngine;
 
 public class FiguresManager : MonoBehaviour
 {
-    private Transform notesTransform;
-    private GameObject notePrefab;
-    private List<Note> notesList;
-    static Vector3 spawnPoint;
-    static Vector3 endPoint;
-    private float timer = 0;
-    private float lastSpawnTime = 0;
-    static BoxCollider2D greenCollider;
+    private Transform _notesTransform;
+    private GameObject _notePrefab;
+    private List<Note> _notesList;
+    static Vector3 _spawnPoint;
+    static Vector3 _endPoint;
+    private float _timer = 0;
+    private float _lastSpawnTime = 0;
+    static BoxCollider2D _greenCollider;
+    private ShapeRecognizer _shapeRecognizer;
 
     private class Note
     {
-        private float timeOfNote;
-        private float duration = 2;
-        public GameObject reference;
+        private readonly float _timeOfNote;
+        private readonly float duration = 2;
+        private readonly GameObject _reference;
 
         public Note(float time, GameObject reference)
         {
-            timeOfNote = time;
-            this.reference = reference;
+            _timeOfNote = time;
+            this._reference = reference;
         }
 
         private float GetY(float currTime)
         {
-            return ((timeOfNote - currTime) / duration) * (spawnPoint.y - endPoint.y);
+            return ((_timeOfNote - currTime) / duration) * (_spawnPoint.y - _endPoint.y);
         }
 
         public void UpdateY(float currTime)
-        { 
-            Vector3 currPos = reference.transform.position;
+        {
+            Vector3 currPos = _reference.transform.position;
             Vector3 newPos = new Vector3(currPos.x, GetY(currTime), currPos.z);
-            reference.transform.position = newPos;
-            // Debug.Log(newPos);
+            _reference.transform.position = newPos;
         }
 
         public bool HasPassedEndpoint()
         {
-            return this.reference.transform.position.y < endPoint.y;
+            return this._reference.transform.position.y < _endPoint.y;
         }
 
         public bool IsInFrontOfScoreZone()
         {
-            return greenCollider.OverlapPoint(this.reference.transform.position);
+            return _greenCollider.OverlapPoint(this._reference.transform.position);
         }
     }
-    
-    
+
 
     void Start()
     {
-        spawnPoint = new Vector3(0, 1000, -5);
-        endPoint = new Vector3(0, -870, -5);
-        this.notesTransform = GameObject.Find("Notes").transform;
-        this.notePrefab = Resources.Load<GameObject>("Prefabs/Dummy");
-        this.notesList = new List<Note>();
-        greenCollider = GameObject.Find("GreenZone").GetComponent<BoxCollider2D>();
+        _shapeRecognizer = GameObject.Find("GameManager").GetComponent<ShapeRecognizer>();
+        _spawnPoint = new Vector3(0, 1000, -5);
+        _endPoint = new Vector3(0, -870, -5);
+        this._notesTransform = GameObject.Find("Notes").transform;
+        this._notePrefab = Resources.Load<GameObject>("Prefabs/Dummy");
+        this._notesList = new List<Note>();
+        _greenCollider = GameObject.Find("GreenZone").GetComponent<BoxCollider2D>();
     }
 
     void Update()
     {
-        this.timer += Time.deltaTime;
-        foreach (Note note in notesList)
-        {
-            note.UpdateY(timer);
-            if (note.IsInFrontOfScoreZone())
-            {
-                Debug.Log("Verde");
-            }
-            // if (note.HasPassedEndpoint())
-            // {
-            //     Destroy(note.reference);
-            //     notesList.Remove(note);
-            // }
-        }
+        this._timer += Time.deltaTime;
+        foreach (Note note in _notesList)
+            note.UpdateY(_timer);
 
-        if (timer - lastSpawnTime >= 2)
+        if (_timer - _lastSpawnTime >= 2)
         {
-            // Debug.Log("Aci!");
-            SpawnNote(timer + 2);
-            lastSpawnTime = timer;
+            SpawnNote(_timer + 2);
+            _lastSpawnTime = _timer;
         }
     }
 
     private GameObject SpawnNote(float time)
     {
-        var newNote = Instantiate(this.notePrefab, new Vector3(0, 1500, -5), Quaternion.identity);
-        newNote.transform.SetParent(this.notesTransform);
-        notesList.Add(new Note(time, newNote));
+        var newNote = Instantiate(this._notePrefab, new Vector3(0, 1500, -5), Quaternion.identity);
+        newNote.transform.SetParent(this._notesTransform);
+        _notesList.Add(new Note(time, newNote));
         return newNote;
+    }
+
+    public void OnNewShape()
+    {
+        string shape = _shapeRecognizer.Shape;
+
+        foreach (Note note in _notesList)
+        {
+            if (note.IsInFrontOfScoreZone())
+            {
+                Debug.Log("Wooo hit a note in the green zone");
+            }
+        }
     }
 }
