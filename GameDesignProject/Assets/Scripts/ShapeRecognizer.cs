8cf725177c73e009f0dab18b740d83b85a11f8f9
csum112 <cataremix15@gmail.com>
2021-05-25 00:03:43 +0300

    added taps to recognizer

--- GameDesignProject/Assets/Scripts/ShapeRecognizer.cs
+++ GameDesignProject/Assets/Scripts/ShapeRecognizer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
@@ -11,6 +12,7 @@ public class ShapeRecognizer : MonoBehaviour
 {
     private DrawPath _drawPath;
     private RecognizerAlgo _recognizer;
+    [SerializeField] private double tapTreshold = 50;
     [SerializeField] private double accuracyTreshold = 0.7;
     public UnityEvent onFigure = new UnityEvent();
 
@@ -24,9 +26,32 @@ public class ShapeRecognizer : MonoBehaviour
         this._drawPath = GameObject.FindWithTag("Playarea").GetComponent<DrawPath>();
     }
 
+    private bool isTap(Vector3[] points)
+    {
+        double sum = 0;
+        Vector3 runningPoint = points.First();
+        foreach (Vector3 point in points)
+        {
+            sum += Vector3.Distance(runningPoint, point);
+            runningPoint = point;
+            if (sum >= tapTreshold)
+                return false;
+        }
+        return true;
+    }
+
     public void OnShapeDrawn()
     {
         Vector3[] points = this._drawPath.GetPoints();
+
+        if (isTap(points))
+        {
+            _figureDrawn = "tap";
+            guessText.SetText(_figureDrawn);
+            onFigure.Invoke();
+            return;
+        }
+            
         var result = _recognizer.Recognize(Vector3ToTimePointF(points));
         guessText.SetText($"{result.Name} - {Math.Round(result.Score * 100)}%");
         if (result.Score >= accuracyTreshold)
